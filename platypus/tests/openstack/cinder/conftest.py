696fa7f9e741bbe78a91232edc24d454aaf7044e
Lars Kellogg-Stedman <lars@redhat.com>
2017-07-14 09:38:00 -0400

    clean up cinder tests

--- platypus/tests/openstack/cinder/conftest.py
+++ platypus/tests/openstack/cinder/conftest.py
@@ -3,6 +3,13 @@ import pytest
 
 try:
     from cinder.common.config import CONF
+    from cinder.objects import volume
+    from cinder.objects import volume_attachment
+
+    try:
+        from cinder.volume.manager import volume_backend_opts
+    except ImportError:
+        from cinder.volume.manager import volume_manager_opts as volume_backend_opts
 except ImportError:
     CONF = None
 
@@ -13,12 +20,18 @@ def pytest_addoption(parser):
 
 def pytest_generate_tests(metafunc):
     if CONF is None:
+        metafunc.parametrize('cinder_conf', None)
         return
 
     CONF(project='cinder',
          args=metafunc.config.option.cinder_arg)
 
+    metafunc.parametrize('cinder_conf', [CONF])
+
     if 'cinder_backend' in metafunc.fixturenames:
-        enabled_backends = CONF.enabled_backends or []
-        metafunc.parametrize('cinder_backend',
-                              filter(None, enabled_backends))
+        enabled_backends = filter(None, CONF.enabled_backends or [])
+        for backend in enabled_backends:
+            CONF.register_opts(volume_backend_opts, group=backend)
+
+        metafunc.parametrize('cinder_backend', enabled_backends)
+
--- platypus/tests/openstack/cinder/test_volume_driver.py
+++ platypus/tests/openstack/cinder/test_volume_driver.py
@@ -1,17 +1,12 @@
 from __future__ import print_function
 import pytest
 
-try:
-    from cinder.common.config import CONF, cfg
-    volume_driver_opt = cfg.StrOpt('volume_driver', default=None)
-except ImportError:
-    CONF = None
+def test_cinder_volume_driver(cinder_conf, cinder_backend):
+    if cinder_conf is None:
+        pytest.skip('cinder: cinder configuration is not available')
 
-@pytest.mark.skipif(CONF is None,
-                    reason='cinder: cinder is not available')
-def test_cinder_volume_driver(cinder_backend):
-    CONF.register_opt(volume_driver_opt, group=cinder_backend)
-    volume_driver = getattr(CONF, cinder_backend).volume_driver
+    volume_driver = getattr(cinder_conf, cinder_backend).volume_driver
+
     assert volume_driver is not None, (
         'cinder: backend %s has no volume_driver configured' % cinder_backend)
     assert not volume_driver.endswith('LVMVolumeDriver'), (
