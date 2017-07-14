519d4b6089a64d562fcea0711c3ac8acc615d601
Lars Kellogg-Stedman <lars@redhat.com>
2017-07-14 14:28:03 -0400

    cli updates

--- platypus/main.py
+++ platypus/main.py
@@ -1,9 +1,12 @@
+import os
 import sys
 import pytest
+
+HERE = os.path.dirname(__file__)
 
 def main():
     args = sys.argv[1:]
     args.append('--assert=plain')
-    args.extend(['--pyargs', __name__.split('.')[0]])
-    print args
-    pytest.main(args)
+    args.append(os.path.join(HERE, 'tests'))
+
+    sys.exit(pytest.main(args))
